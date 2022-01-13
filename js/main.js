1d2f26f1fe10f6f68522b5b5a244eaf5ae2820a5
Allen Manoj <allenmanoj17@gmail.com>
2022-01-14 02:22:00 +0530

    update and add feature

--- js/main.js
+++ js/main.js
@@ -370,3 +370,27 @@
 	};
 })();
 
+
+$.Tween.propHooks.number = {
+	get: function(tween) {
+	  var num = tween.elem.innerHTML.replace(/^[^\d-]+/, "");
+	  return parseFloat(num) || 0;
+	},
+  
+	set: function(tween) {
+	  var opts = tween.options;
+	  tween.elem.innerHTML =
+		(opts.prefix || "") +
+		tween.now.toFixed(opts.fixed || 0) +
+		(opts.postfix || "");
+	}
+  };
+  
+  $("#num-1").animate(
+	{ number: 25 },
+	{
+	  duration: 7000,
+	  postfix: "+"
+	}
+  );
+  
